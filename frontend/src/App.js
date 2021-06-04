160a874815f45db7babb5ad2ea91a52ce42449d9
munsif12 <munsifalimisri69@gmail.com>
2021-06-04 23:35:48 +0500

    built basic front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welocme to auth based e-commerce app with react-redux</h1>
+    </div>
+  );
+}
+
+export default App;
