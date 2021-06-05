a39b3a4530eebae1b83ab6c6ecafaa921066bc42
munisf12 <munsifalimisri69@gmail.com>
2021-06-05 23:55:19 +0500

    routes related to login and registration done

---
+++ backend/routes/loginRegisterRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const Router = express.Router();
+const {
+  loginController,
+  registerController,
+} = require("../controller/loginRegisterController");
+Router.get("/login", loginController);
+Router.post("/register", registerController);
+module.exports = Router;
