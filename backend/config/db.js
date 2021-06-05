16457fe3ae864078ba8ef93de2bb42f906bd2b14
munisf12 <munsifalimisri69@gmail.com>
2021-06-05 23:54:06 +0500

    db connection work done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,22 @@
-const mongoose = reuire("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose
-  .connect(process.env.DB_CONN_URL, {})
-  .then(() => {
-    console.log("Connection to mongoDB success");
-  })
-  .catch((error) => {
+const connection = () => {
+  try {
+    mongoose
+      .connect(process.env.DB_CONN_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Connection to mongoDB success");
+      })
+      .catch((error) => {
+        console.log(`Connection to mongoDB failed ${error}`);
+        process.exit(1);
+      });
+  } catch (error) {
     console.log(`Connection to mongoDB failed ${error}`);
-  });
+    process.exit(1);
+  }
+};
+module.exports = connection;
