61c4c620087b6be290601ea32bb8e8cb82821c43
munisf12 <munsifalimisri69@gmail.com>
2021-06-05 23:55:45 +0500

    main server file

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+
+//importing routes
+const loginRegisterRoute = require("./routes/loginRegisterRoute");
+//making connection to db
+const connection = require("./config/db");
+connection();
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-  res.json({ message: "running " });
-});
+//middlewares
+app.use(express.json());
+//routes middleware
+app.use("/user", loginRegisterRoute);
 app.listen(PORT, () => {
   console.log(`Server runnig successfully on port : ${PORT}`);
 });
