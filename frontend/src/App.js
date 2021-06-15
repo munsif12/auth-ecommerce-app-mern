82e9b1c184f55ce4cf987daaed696f2b28dc89e8
munisf12 <munsifalimisri69@gmail.com>
2021-06-15 23:45:56 +0500

    Ui change added todo and on going work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Welocme to auth based e-commerce app with react-redux</h1>
+      <h2>User Backend part done => backend</h2>
+      <h2>Wonrking on products route => backend</h2>
     </div>
   );
 }
