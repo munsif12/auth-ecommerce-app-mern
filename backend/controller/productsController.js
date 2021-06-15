26ebf9615a4399b83eded591c2eb0cddd5151c58
munsif12 <munsifalimisri69@gmail.com>
2021-06-15 15:30:27 +0500

    working on all products route

--- backend/controller/productsController.js
+++ backend/controller/productsController.js
@@ -1,5 +1,19 @@
+const product = require("../models/productModel");
 const getAllProductsController = (req, res) => {
-  res.json({ message: "success" });
+  try {
+    const allproducts = await product.find({});
+    if (allproducts) {
+      res.status(200).json({
+        data: allproducts,
+      });
+    } else {
+      res.status(400).json({
+        message: "No Product available right now",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 const getproductController = (req, res) => {
   res.json({ productId: req.params.id });
