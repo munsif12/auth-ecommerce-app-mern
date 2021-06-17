fa3575e0562c8b9849b9acf507462fd1ddb4ce9a
munisf12 <munsifalimisri69@gmail.com>
2021-06-18 00:21:43 +0500

    added user detaiils controller

---
+++ backend/controller/userDetailsController.js
@@ -0,0 +1,29 @@
+const userDetails = require("../models/userDetails");
+
+const addNewUserDetail = async (req, res) => {
+  try {
+    const user = await userDetails.create(req.body);
+    res.json({ message: "New User Added Successfully", user });
+  } catch (error) {
+    console.log(error);
+    res.json({ error: error.message });
+  }
+};
+const fetchUsersDetails = async (req, res) => {
+  try {
+    res.json({ productId: "User Filteration successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ error: error.message });
+  }
+};
+
+const fetchUserDetail = async (req, res) => {
+  try {
+    res.json({ productId: "User Detail successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ error: error.message });
+  }
+};
+module.exports = { fetchUsersDetails, addNewUserDetail, fetchUserDetail };
