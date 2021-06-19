f91d351dd8a11c9b7edf004f31e9d8da85fcc2b8
munsif12 <munsifalimisri69@gmail.com>
2021-06-20 00:21:43 +0500

    added advanced filtering like quering in objects, arrays and also added sorting

--- backend/controller/userDetailsController.js
+++ backend/controller/userDetailsController.js
@@ -1,20 +1,36 @@
 const userDetails = require("../models/userDetailsModel");
 
+//concept of advance filtering added in this file like
+/*  
+   1 => price filterign like product price >= 200 && <= 100  ==> collectin.find({productPrice:{$gte:200,$lte:1000}})
+   2=> quering in object ==> collectin.find({"city.street":"street no 22"})
+   3 => quering and array ==> collectin.find({favProducts:{$in:['iphone','window']}}) 
+   4 => filtering and then sorting ==> collectin.find({productPrice:{$gte:200,$lte:1000}}).sort("-productPrice") +=> ( - ) minus for desc bydefault ascen without -*/
 const addNewUserDetail = async (req, res) => {
   try {
     const user = await userDetails.create(req.body);
     res.json({ message: "New User Added Successfully", user });
   } catch (error) {
     console.log(error);
-    res.json({ error: error.message });
+    res.status(404).json({ error: error.message });
   }
 };
 const fetchUsersDetails = async (req, res) => {
   try {
-    res.json({ productId: "User Filteration successfully" });
+    const { sort, ...restQuery } = req.query;
+    console.log(restQuery);
+    const query = JSON.stringify(restQuery);
+    const addDollerSign = query.replace(
+      /\b(gt|lt|lte|gte|in|all)\b/g,
+      (match) => `$${match}`
+    );
+    const user = await userDetails.find(JSON.parse(addDollerSign)).sort(sort);
+    /*/favProducs: { $in: ["windows","iphone"] }, to search in array => $in op  works like or operator and $all work like and 
+    op ka jis documeent ka favProducts ka array ma window or iphone ho sirf wohe document send kro*/
+    res.json({ message: "Success", users: user });
   } catch (error) {
     console.log(error);
-    res.json({ error: error.message });
+    res.status(404).json({ error: error.message });
   }
 };
 
@@ -23,7 +39,7 @@ const fetchUserDetail = async (req, res) => {
     res.json({ productId: "User Detail successfully" });
   } catch (error) {
     console.log(error);
-    res.json({ error: error.message });
+    res.status(404).json({ error: error.message });
   }
 };
 module.exports = { fetchUsersDetails, addNewUserDetail, fetchUserDetail };
