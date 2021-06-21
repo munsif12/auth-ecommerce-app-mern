f719f7238af222cc6a2a5bf51f6d727bfd3cd869
munsif12 <munsifalimisri69@gmail.com>
2021-06-21 23:47:36 +0500

    Updated msg for user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,30 @@ function App() {
   return (
     <div className="App">
       <h1>Welocme to auth based e-commerce app with react-redux</h1>
-      <h2>User Backend part done backend</h2>
-      <h2>Wonrking on products route backend</h2>
+      <h2>Working on Back-end Front-end Comming soon</h2>
+      <h3>Topics added to backend</h3>
+      <ol>
+        <li>
+          <h4>userAuthRoute,userDetailsRoute,productRouter</h4>
+        </li>
+        <li>
+          <h4>
+            User Authentication used jwt and hashing user password using bcrypt
+          </h4>
+        </li>
+        <li>
+          <h4>Filtering || Serching for product and user both</h4>
+        </li>
+        <li>
+          <h4>Sorting</h4>
+        </li>
+        <li>
+          <h4>Fields selecting shows only required fileds</h4>
+        </li>
+        <li>
+          <h4>MORE TO COME ....</h4>{" "}
+        </li>
+      </ol>
     </div>
   );
 }
