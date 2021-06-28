9fe29bded6a93a97a2eac7340651c17c2f373ef2
munsif12 <munsifalimisri69@gmail.com>
2021-06-29 00:43:39 +0500

    added middleware to protect private routes

--- backend/routes/userDetailsRoutes.js
+++ backend/routes/userDetailsRoutes.js
@@ -5,9 +5,10 @@ const {
   fetchUsersDetails,
   addNewUserDetail,
   fetchUserDetail,
+  protectAuthMidd,
 } = require("../controller/userDetailsController");
 
-Router.get("/", fetchUsersDetails);
+Router.get("/", protectAuthMidd, fetchUsersDetails);
 Router.post("/new-user", addNewUserDetail);
 Router.get("/:_id", fetchUserDetail); //to get  single user
 
