473f4c8e7684746b0828fd0982360733d961c43f
munsif12 <munsifalimisri69@gmail.com>
2021-07-01 23:32:58 +0500

    added a new route

--- backend/routes/loginRegisterRoute.js
+++ backend/routes/loginRegisterRoute.js
@@ -3,9 +3,12 @@ const Router = express.Router();
 const {
   loginController,
   registerController,
+  fetchUsers,
+  protectAuthMidd,
 } = require("../controller/loginRegisterController");
 
 Router.post("/login", loginController);
 Router.post("/register", registerController);
+Router.get("/", protectAuthMidd, fetchUsers);
 
 module.exports = Router;
