02f0fb3f0389002e0f8929601374b19384ad720c
munsif12 <munsifalimisri69@gmail.com>
2021-07-02 23:35:38 +0500

    apiFeature removed and moved to utils function

--- backend/controller/userDetailsController.js
+++ backend/controller/userDetailsController.js
@@ -1,5 +1,5 @@
 const userDetails = require("../models/userDetailsModel");
-// const ApiFeatures = require("../utility/commonApiFeature");
+const ApiFeatures = require("../utility/commonApiFeature");
 const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
 require("dotenv").config();
@@ -14,58 +14,6 @@ require("dotenv").config();
    6 => Pagination
    7 => Code cleanups Or using class to reuse the code in multidiff files
    -*/
-
-class ApiFeatures {
-  constructor(model, query) {
-    this.model = model;
-    this.queryParams = query;
-    this.query = null;
-  }
-  filtration() {
-    try {
-      const { sort, fields, page, limit, ...restQuery } = this.queryParams;
-      this.query = JSON.stringify(restQuery);
-      const addDollerSign = this.query.replace(
-        /\b(gt|lt|lte|gte|in|all)\b/g,
-        (match) => `$${match}`
-      );
-      this.query = userDetails.find(JSON.parse(addDollerSign));
-      return this;
-    } catch (error) {
-      res.status(401).json({
-        error: error.message,
-      });
-    }
-  }
-  sort() {
-    if (this.queryParams.sort) {
-      const sortOn = this.queryParams.sort.split(",").join(" "); //working ParamsComming as ( name,age ) with comma we have to remove them now .split => [ name , age].join(" ") ==> result (name age)  here we removed the comma becoz for sorting and selecting the query is model.select("name agr")
-      this.query = this.query.sort(sortOn); //still not resolving the promise here bcoz we have one more method to chain which is .select for selecting required firlds
-    } else {
-      this.query = this.query.sort("createdAt"); //still not resolving the promise here bcoz we have one more method to chain which is .select for selecting required firlds
-    }
-    return this;
-  }
-  fieldLimitation() {
-    if (this.queryParams.fields) {
-      // console.log(fileds);
-      const fieldsConToStr = this.queryParams.fields.split(",").join(" "); //working ParamsComming as ( name,age ) with comma we have to remove them now .split => [ name , age].join(" ") ==> result (name age)  here we removed the comma becoz for sorting and selecting the query is model.select("name agr")
-      this.query = this.query.select(fieldsConToStr);
-      // console.log(await user);
-    }
-    return this;
-  }
-  pagination() {
-    const reqLimit = this.queryParams.limit || 2;
-    const reqPage = this.queryParams.page || 1;
-    const skipValues = (reqPage - 1) * reqLimit;
-    this.query = this.query.skip(skipValues).limit(Number(reqLimit));
-    return this;
-  }
-  get() {
-    return this.query;
-  }
-}
 const addNewUserDetail = async (req, res) => {
   try {
     const user = await userDetails.create(req.body);
@@ -167,12 +115,16 @@ async function protectAuthMidd(req, res, next) {
     console.log(tokenVarification);
     //4 check if user exists in db
     const user = await userDetails.findById({ _id: tokenVarification.id });
-
+    if (!user) {
+      res
+        .status(401)
+        .json({ error: "user belonging to this email doesnt exists" });
+    }
     next();
   } catch (error) {
     res.status(401).json({ error: error.message });
   }
-} //now you can use this middleware for authentication anywherer in the codebase i have already used it on productRoutes.js
+} //now you can use this middleware for authentication anywherer in the codebase just change the model name when finding the user with emil
 
 module.exports = {
   fetchUsersDetails,
