1d68028e745e5175b7c310ae69478b30b558ec64
munsif12 <munsifalimisri69@gmail.com>
2021-08-11 00:39:01 +0500

    pkg fiiles

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,17 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cookieParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit"); //to prevent brute force attacks
+const mongoSanitize = require("express-mongo-sanitize"); //to prevent sql query attack
+var xss = require("xss-clean"); //to prevent from injecting scripts like html scripts (alert etc)
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour minutes
+  max: 1000, // limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again after 1 hour. ",
+});
+
+//  apply to all requests
+
 //importing routes
 const loginRegisterRoute = require("./routes/loginRegisterRoute");
 const productsRoutes = require("./routes/productsRoutes");
@@ -13,6 +24,9 @@ const PORT = process.env.PORT || 5000;
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(limiter);
+app.use(mongoSanitize()); //must come after express.json()
+app.use(xss());
 //routes middleware
 app.use("/auth", loginRegisterRoute);
 app.use("/user/details", userDetails);
