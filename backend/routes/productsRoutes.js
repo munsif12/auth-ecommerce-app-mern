c90bf59727d676d84a6ca729772aa255be5b9cbf
munsif12 <munsifalimisri69@gmail.com>
2021-08-12 01:13:19 +0500

    added like-dislike product route and also added authentication middlewares+userrolebaseAuth URBA

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const Router = express.Router();
+require("../models/userAuthModel");
 
 const {
   getAllProductsController,
   addProduct,
   getproductController,
+  likeProduct,
+  dislikeProduct,
 } = require("../controller/productsController");
+const {
+  protectAuthMidd,
+  AuthenticateRole,
+} = require("../controller/loginRegisterController");
 
-Router.get("/", getAllProductsController);
-Router.post("/add-product", addProduct);
-Router.get("/:_id", getproductController);
+Router.get("/", getAllProductsController); //everyone can access this route login or not login
+Router.post(
+  "/add-product",
+  protectAuthMidd,
+  AuthenticateRole("artist"),
+  addProduct
+);
+Router.get("/:id", getproductController);
+Router.post(
+  "/:id/like-product",
+  protectAuthMidd,
+  AuthenticateRole("artist", "buyer"),
+  likeProduct
+);
+Router.post(
+  "/:id/dislike-product",
+  protectAuthMidd,
+  AuthenticateRole("artist", "buyer"),
+  dislikeProduct
+);
 
 module.exports = Router;
 // Date: Tue Jun 8 10:47:45 2021 +0500
