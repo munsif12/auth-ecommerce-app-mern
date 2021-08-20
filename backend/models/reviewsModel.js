a5b5203b636b702b693d0c571580837907e09ce3
munsif12 <munsifalimisri69@gmail.com>
2021-08-20 05:00:47 +0500

    before getting product added reviews and inside reviews populated user who reviewed the product

--- backend/models/reviewsModel.js
+++ backend/models/reviewsModel.js
@@ -7,12 +7,22 @@ const reviewsSchema = mongoose.Schema(
       min: 0,
       max: 5,
     },
-    reviewedBy: mongoose.Schema.ObjectId,
-    productId: mongoose.Schema.ObjectId,
+    reviewedBy: {
+      type: mongoose.Schema.ObjectId,
+      ref: "user",
+    },
+    productId: mongoose.Schema.ObjectId, //child to parent relationship q ka product k ander reviews aty h
   },
   {
     timestamps: true,
   }
 );
+reviewsSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "reviewedBy",
+    select: "name email",
+  });
+  next();
+});
 const review = new mongoose.model("review", reviewsSchema);
 module.exports = review;
